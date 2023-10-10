246d6163255a0b84302d3aa83e4fcdfbcc1e7116
cnewbywa <cnewbywa@gmail.com>
2023-10-10 17:53:36 +0300

    Made changes to networking and how to connect to some of the containers

---
+++ docker/runtime/auth/start-auth.sh
@@ -0,0 +1,1 @@
+docker compose --env-file=/home/c/docker/container_configs/keycloak/.env up --force-recreate --remove-orphans
---
+++ docker/runtime/network/create-network.sh
@@ -0,0 +1,1 @@
+docker network create --driver=bridge --subnet=172.31.0.0/16 internal-network
